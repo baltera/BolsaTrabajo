f1acf96bdf0e4bbd08b1f4e3256b5b68fe1de0cf
baltera <baltera@users.noreply.github.com>
2015-05-12 14:29:17 -0600

    Initial commit

---
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/MainActivity.java
@@ -0,0 +1,33 @@
+package fia.ues.sv.bolsatrabajo;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+    String[] menu={"FB12001","FR12001","RG12001","RS07003","SB12002"};
+    String[] activities={"FB12001Activity","FR12001Activity","RG12001Activity","RS07003Activity","SB12002Activity"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id){
+        super.onListItemClick(l, v, position, id);
+            String nombreValue=activities[position];
+            try{
+                Class<?>
+                        clase=Class.forName("fia.ues.sv.bolsatrabajo."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }
+    }
+}
