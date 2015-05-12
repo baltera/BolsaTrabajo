500b968c8bdcff92edf80ea4d8e50e137e6978bd
baltera <baltera@users.noreply.github.com>
2015-05-12 14:36:48 -0600

    Un comentario de como trabajaremos el proyecto

--- app/src/main/java/fia/ues/sv/bolsatrabajo/MainActivity.java
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
     String[] menu={"FB12001","FR12001","RG12001","RS07003","SB12002"};
+    //Cada uno debe crear sus activities con el numero de carnet para ahí poner los CRUD de sus tablas
     String[] activities={"FB12001Activity","FR12001Activity","RG12001Activity","RS07003Activity","SB12002Activity"};
 
     @Override
