ab5b6107a62b2e94ef61e6cfe664d616d423df79
rafael31194 <rafael31194@hotmail.com>
2015-05-13 12:11:07 -0500

    hice mis layouts de las tablas

--- app/src/main/java/fia/ues/sv/bolsatrabajo/SB12002Activity.java
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/SB12002Activity.java
@@ -1,38 +1,37 @@
 package fia.ues.sv.bolsatrabajo;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class SB12002Activity extends ActionBarActivity {
+public class SB12002Activity extends ListActivity {
+    String[] menu={"GradoEspecializacion","Referencia"};
+    String[] activities={"GradoEspecializacionActivity","ReferenciaActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sb12002);
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu));
+        setTitle("Tablas");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sb12002, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String optionValue=activities[position];
+        try{
+            Class <?> clase=Class.forName("fia.ues.sv.bolsatrabajo."+optionValue);
+            Intent intento=new Intent(this,clase);
+            startActivity(intento);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
