a27445935ea4a7c4f7037f36c1f5a0651a09b85f
Mónica Flores <monicaflores_207@hotmail.com>
2015-05-14 00:18:18 -0600

    FB12001Activity AplicacionMenuActivity modificada

---
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/AplicacionMenuActivity.java
@@ -0,0 +1,39 @@
+package fia.ues.sv.bolsatrabajo;
+
+import android.app.ListActivity;
+import android.content.Intent;
+//import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class AplicacionMenuActivity extends ListActivity {
+    String[] submenu={"Insertar Registro","Eliminar Registro","Consultar Registro","Actualizar Registro"};
+    String[] activities={"AplicacionInsertarActivity","AplicacionEliminarActivity","AplicacionConsultarActivity","AplicacionConsultarActivity"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //ListView listView = getListView();
+        //ponerle color
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,submenu);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick (ListView l,View v, int position, long id){
+        super.onListItemClick(l,v,position,id);
+        String nombreValue=activities[position];
+        //l.getChildAt(position).setBackgroundColor(Color.rgb(128,128,255));
+        try{
+            Class<?> clase=Class.forName("fia.ues.sv.bolsatrabajo."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+}
