2d2d037bc90700e43b43515a3df6c4342968985b
Mónica Flores <monicaflores_207@hotmail.com>
2015-05-15 00:42:56 -0600

    layouts de aplicacion y cargo con sus respectivos menus

--- app/src/main/java/fia/ues/sv/bolsatrabajo/FR12001Activity.java
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/FR12001Activity.java
@@ -11,7 +11,7 @@ public class FR12001Activity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fr120012);
+        setContentView(R.layout.activity_fr12001);
     }
 
 
