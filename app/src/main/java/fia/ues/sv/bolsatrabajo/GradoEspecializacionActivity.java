b15dfe985ab27195459ae1a81b59f77e7b87cc5b
rafael31194 <rafael31194@hotmail.com>
2015-05-15 14:42:01 -0500

    Agregue los layout de edgar al String.xml  le cambie la imagen al boton insertar, hice que cada imgButton me muestre el layout correspondiente

--- app/src/main/java/fia/ues/sv/bolsatrabajo/GradoEspecializacionActivity.java
+++ app/src/main/java/fia/ues/sv/bolsatrabajo/GradoEspecializacionActivity.java
@@ -1,9 +1,11 @@
 package fia.ues.sv.bolsatrabajo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class GradoEspecializacionActivity extends ActionBarActivity {
@@ -14,25 +16,45 @@ public class GradoEspecializacionActivity extends ActionBarActivity {
         setContentView(R.layout.activity_grado_especializacion);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_grado_especializacion, menu);
-        return true;
+    public void insertarReferencia(View view) {
+
+        try{
+            Class <?> clase=Class.forName("fia.ues.sv.bolsatrabajo.GradoEspecializacionInsertarActivity");
+            Intent intento=new Intent(this,clase);
+            startActivity(intento);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void eliminarReferencia(View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try{
+            Class <?> clase=Class.forName("fia.ues.sv.bolsatrabajo.GradoEspecializacionEliminarActivity");
+            Intent intento=new Intent(this,clase);
+            startActivity(intento);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void modificarReferencia(View view) {
+        try{
+            Class <?> clase=Class.forName("fia.ues.sv.bolsatrabajo.GradoEspecializacionModificarActivity");
+            Intent intento=new Intent(this,clase);
+            startActivity(intento);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void consultarReferencia(View view) {
+        try{
+            Class <?> clase=Class.forName("fia.ues.sv.bolsatrabajo.GradoEspecializacionConsultarActivity");
+            Intent intento=new Intent(this,clase);
+            startActivity(intento);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
